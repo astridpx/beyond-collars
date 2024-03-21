b9a89b547a0a251caf4a8f4c4a54a2a4bcdf2738
Iskyy <lanteangelo12@gmail.com>
2024-03-21 20:13:26 +0800

    fix home navlink

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,7 +12,7 @@
 
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" style="color: white;" href="#">Home</a>
+                    <a class="nav-link" style="color: white;" href="/bc/user">Home</a>
                 </li>
                 <!-- <li class="nav-item">
                     <a class="nav-link" style="color: white;" href="/bc/user/event.php">Event</a>
